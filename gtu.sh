193e18239139772596c7b2d53a0de510d311a542
tangcheng <tangcheng15911@sina.com>
2016-04-10 17:43:27 +0800

    add options

--- gtu.sh
+++ gtu.sh
@@ -1,6 +1,21 @@
 #! /bin/bash
 
-#gbk转utf-8
+from='gbk'
+to='utf-8'
+while getopts f:t:
+do
+    case "$opt" in
+        f)
+            from=$OPTARG;;
+        t)
+            to=$OPTARG;;
+        h)
+            echo "参数:"
+            echo "-h        show this help"
+            echo "-f        from charset default gbk"
+            echo "-t        to charset default utf-8"
+    esac        
+done
 IFSbak=$IFS
 IFS="~"	#这里用一个不常用的符号就好
 while read svnResult
@@ -10,7 +25,7 @@ do
 	svnResult=${svnResult//\}/\\\}}
 	svnResult=${svnResult//\$/\\\$}
 	svnResult=${svnResult//\*/\\\*}
-	paramString="echo mb_convert_encoding(\"""${svnResult}""\",\"utf-8\",\"gbk\")"";"
+	paramString="echo mb_convert_encoding(\"""${svnResult}""\",\"${to}\",\"${from}\")"";"
 	result=`php -r "$paramString"`
 	result=${result//\\\"/\"}
 	result=${result//\\\{/\{}
